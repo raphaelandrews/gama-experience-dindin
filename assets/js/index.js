0620a4fdc7ea6084f4babbfefee0b82ff73c0018
Raphael Andrews <raphaelandrews95@gmail.com>
2022-10-17 23:32:01 -0300

    dialog fix

--- assets/js/index.js
+++ assets/js/index.js
@@ -46,21 +46,33 @@ modalClose.forEach((mc) => {
 const showModal = document.querySelector('#btn1')
 const showModal2 = document.querySelector('#btn2')
 const showModal3 = document.querySelector('#btn3')
-const close = document.querySelector('#close')
-const dialog = document.querySelector('dialog')
+const close1 = document.querySelector('#close1')
+const close2 = document.querySelector('#close2')
+const close3 = document.querySelector('#close3')
+const dialog1 = document.querySelector('#dialog1')
+const dialog2 = document.querySelector('#dialog2')
+const dialog3 = document.querySelector('#dialog3')
 
 showModal.addEventListener('click', e => {
-  dialog.showModal()
+  dialog1.showModal()
 })
 
 showModal2.addEventListener('click', e => {
-  dialog.showModal()
+  dialog2.showModal()
 })
 
 showModal3.addEventListener('click', e => {
-  dialog.showModal()
+  dialog3.showModal()
 })
 
-close.addEventListener('click', e => {
-  dialog.close()
+close1.addEventListener('click', e => {
+  dialog1.close()
+})
+
+close2.addEventListener('click', e => {
+  dialog2.close()
+})
+
+close3.addEventListener('click', e => {
+  dialog3.close()
 })
